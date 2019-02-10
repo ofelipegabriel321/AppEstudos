594302c66edfdd4faab818f71672b07f5f435426
ofelipegabriel <ofelipegabriel321@gmail.com>
2019-02-10 15:44:04 -0300

    Modularização do código. Criação do primeiro Controller (LoginController.java). Criação do CadastroActivity. Primeiro Intent para mudar da Activity LoginActivity para CadastroActivity quando o btnCadastroEmLogin for clicado.

--- AppEstudos/app/src/main/java/com/example/projetofinal/dal/App.java
+++ AppEstudos/app/src/main/java/com/example/projetofinal/dal/App.java
@@ -6,7 +6,9 @@ import com.example.projetofinal.models.MyObjectBox;
 
 import io.objectbox.BoxStore;
 
+// Depois de finalizar a classe, vá na AndroidManifest.xml e faça o código android:name="dal.App.xml"
 public class App extends Application {
+    // Um BoxStore é a interface que se liga diretamente com o banco de dados
     private BoxStore boxStore;
 
     @Override
@@ -20,6 +22,11 @@ public class App extends Application {
     }
 
     private void inicializarObjectBox(){
+        // OBS: Deve ser criada uma classe e executar o app antes de escrever o código abaixo,
+        // só assim o MyObjectBox vai poder ser chamado.
+        // Instancia a classe BoxStore do MyObjectBox no atributo boxStore. O builder serve para
+        // que seja facilitada a criação do boxStore.
         boxStore = MyObjectBox.builder().androidContext(this).build();
     }
+
 }
--- AppEstudos/app/src/main/java/com/example/projetofinal/ui/LoginActivity.java
+++ AppEstudos/app/src/main/java/com/example/projetofinal/ui/LoginActivity.java
@@ -1,63 +1,112 @@
 package com.example.projetofinal.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.projetofinal.R;
+import com.example.projetofinal.controllers.LoginController;
+import com.example.projetofinal.dal.App;
 
-import java.util.Objects;
+import io.objectbox.BoxStore;
 
 public class LoginActivity extends AppCompatActivity {
 
-    TextInputEditText txtNomeDeUsuario, txtSenha;
-    Button btnLogin;
+    private TextInputEditText txtNomeDeUsuario, txtSenha;
+    private Button btnLogin;
+    private Button btnCadastroEmLogin;
+    private BoxStore boxStore;
+    private LoginController loginController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        componentesLogin();
-        clickLogin();
+        ligarAtributosComViews();
+        tornarBtnLoginClicavelEFuncional();
+        tornarBtnCadastroEmLoginClicavelEFuncional();
+        instanciarBoxStore();
+        instanciarLoginController();
     }
 
-    public void componentesLogin(){
+    private void ligarAtributosComViews(){
         txtNomeDeUsuario = findViewById(R.id.idEmailEmLogin);
         txtSenha = findViewById(R.id.idSenhaEmLogin);
         btnLogin = findViewById(R.id.idBtnLogin);
+        btnCadastroEmLogin = findViewById(R.id.idBtnCadastroEmLogin);
     }
 
-    public void clickLogin(){
+    private void tornarBtnLoginClicavelEFuncional(){
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                login();
+                logar();
             }
         });
     }
 
+    private void tornarBtnCadastroEmLoginClicavelEFuncional(){
+        btnCadastroEmLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                irParaCadastroActivity();
+            }
+        });
+    }
+
+
     /*
-    A classe login está analisando se a String de txtNomeDeUsuario está vazia por
+    A classe logar está analisando se a String de txtNomeDeUsuario está vazia por
     meio do método isEmpty. O trim() serve para fazer com que espaços no começo e
     final da String sumam caso existam.
     Em algum caso da String do Usuario ou da Senha estar vazia, será exibido uma
     mensagem com o método setError.
     */
-    public void login(){
-        if(TextUtils.isEmpty(Objects.requireNonNull(txtNomeDeUsuario.getText()).toString().trim())){
-            txtNomeDeUsuario.setError("Os campos não podem estar vazios");
-        }
-        else if (TextUtils.isEmpty(Objects.requireNonNull(txtSenha.getText()).toString().trim())){
-            txtSenha.setError("Os campos não podem estar vazios");
-        }
-        else{
+
+    private void instanciarBoxStore(){
+        // O ((App) getApplication()).getBoxStore() faz com que o BoxStore possa
+        // ser obtido.
+        boxStore = ((App) getApplication()).getBoxStore();
+    }
+
+    private void instanciarLoginController(){
+        // Agora passamos a boxStore para a Login Activity formar a Box de Estudante
+        loginController = new LoginController(boxStore);
+    }
+
+    public void logar(){
+        if (verificarSePodeLogar()){
             Toast.makeText(getApplicationContext(), "Login feito com sucesso!", Toast.LENGTH_SHORT).show();
+            irParaPainelDoEstudanteActivity();
         }
+    }
+
+    public boolean verificarSePodeLogar(){
+        if (loginController.verificarSeTextoEstaVazio(txtNomeDeUsuario) ||
+                loginController.verificarSeTextoEstaVazio(txtSenha)) {
+            if (loginController.verificarSeTextoEstaVazio(txtNomeDeUsuario)) {
+                txtNomeDeUsuario.setError("Os campos não podem estar vazios");
+            }
+            if (loginController.verificarSeTextoEstaVazio(txtSenha)) {
+                txtSenha.setError("Os campos não podem estar vazios");
+            }
+            return false;
+        }
+        return true;
+    }
+
+    public void irParaPainelDoEstudanteActivity(){
+        // AGORA IR PRA A OUTRA ACTIVITY
+    }
+
+    public void irParaCadastroActivity(){
+        Intent i = new Intent(this, CadastroActivity.class);
+        startActivity(i);
     }
 
 }
